2413c0b7f788729d10e68c8b44d39cc9da44b2a4
Mihailoolegovich <mihailoolegovich@gmail.com>
2022-11-03 21:45:49 +0200

    back-end connection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import calculateString from 'calculate-string';
 
 const calcSignsBtn = ['+', '-', '*', '/', '.'];
 const calcNumBtn = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://test-task-calculate.herokuapp.com/';
 
 export const App = () => {
   const [output, setOutput] = useState('');
@@ -13,6 +17,17 @@ export const App = () => {
   const [baseData, setBaseData] = useState([]);
 
   useEffect(() => {
+    axios
+      .get('/api/calculator')
+      .then(response => setBaseData(response.data.calc[0].value));
+  }, []);
+
+  useEffect(() => {
+    if (baseData.length > 0) {
+      axios.put(`/api/calculator/635ac386ebfa214d4dfaff23`, {
+        value: baseData,
+      });
+    }
     setPosition(baseData.length - 1);
   }, [baseData]);
 
@@ -78,32 +93,44 @@ export const App = () => {
     }
   }
 
+  function cleanHistory() {
+    setBaseData(['']);
+    setCalculation('');
+    setOutput('');
+  }
   return (
     <>
       <div className="container">
         <div className="inputs_tumba">
-          <button
-            className="history_btn"
-            onClick={() => setPosition(position - 1)}
-            disabled={position === 0}
-          >
-            <img
-              width={'30px'}
-              alt={'emoji '}
-              src={'https://cdn-icons-png.flaticon.com/512/565/565344.png'}
-            />
-          </button>
-          <button
-            className="history_btn"
-            onClick={() => setPosition(position + 1)}
-            disabled={position === baseData.length - 1}
-          >
-            <img
-              width={'30px'}
-              alt={'emoji '}
-              src={'https://cdn-icons-png.flaticon.com/512/565/565331.png'}
-            />
-          </button>
+          <div className="inputs_tumba--buttons">
+            <button className="history_btn" onClick={() => cleanHistory()}>
+              Clear History
+            </button>
+            <div>
+              <button
+                className="history_btn"
+                onClick={() => setPosition(position - 1)}
+                disabled={position === 0}
+              >
+                <img
+                  width={'30px'}
+                  alt={'emoji '}
+                  src={'https://cdn-icons-png.flaticon.com/512/565/565344.png'}
+                />
+              </button>
+              <button
+                className="history_btn"
+                onClick={() => setPosition(position + 1)}
+                disabled={position === baseData.length - 1}
+              >
+                <img
+                  width={'30px'}
+                  alt={'emoji '}
+                  src={'https://cdn-icons-png.flaticon.com/512/565/565331.png'}
+                />
+              </button>
+            </div>
+          </div>
           <p className="output">{output || '0'}</p>
           <p className="calc">{calculation || ''}</p>
         </div>
